5abbf4ee46bcab2250828caa3686f153b5fa523b
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-01-18 14:17:29 -0700

    coding challenge 3 complete

--- fundamentals_1/3.js
+++ fundamentals_1/3.js
@@ -1,14 +1,16 @@
 const dolphinAve = (97 + 112 + 101) / 3;
-const koalasAve = (109, 95, 106) / 3;
+const koalasAve = (109 + 95 + 106) / 3;
 
 console.log(dolphinAve, koalasAve)
 
-if(dolphinAve > koalasAve && dolphinAve >= 100) {
+if(dolphinAve > koalasAve && dolphinAve >= 100 ) {
   console.log(`The Dolphn team won the competition with an average score of
   ${Math.floor(dolphinAve)}, and the Koalas Team lost with an average score of ${Math.floor(koalasAve)}.`)
 } else if(koalasAve > dolphinAve && koalasAve >= 100) {
   console.log(`The Koalas Team won the competition with an average score of ${Math.floor(koalasAve)}. The Dolphn team lost with an average score of
   ${Math.floor(dolphinAve)}`)
-} else if(dolphinAve === koalasAve) {
+} else if(dolphinAve === koalasAve && dolphinAve >=100 && koalasAve >= 100) {
   console.log(`There has been a tie between the Dolphins and the Koalas!`)
-}
+} else {
+  console.log("no winner, both below 100")
+}//the last scenario would logically be a tie so no need for the else block
