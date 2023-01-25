6fd1e9d04960eaddb15a76030c2e020fd3641a4c
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-01-25 12:37:39 -0700

    histroy of Javascript

---
+++ fundamentals_1/history.js
@@ -0,0 +1,21 @@
+/* Javascript has BACKWARDS COMPATIBILITY in order to not break the web.
+
+this means that old JS code from 1997 can still work no issue in 2020+ beacause 
+
+evey year they add incremental additons to the language and never remove anything
+
+webistes can work forever without breaking, 
+
+BUT there is a ton of weird bugs sometimes.
+
+The JS is not forwards compatiiblity, so how do we use modern JS today
+
+  1. Developement pahse: use th emost lattest tools in google chrome borwser
+  2. Production: we cnat control which browser a user picks, might not be modern 
+
+  ES5 is fully supported in all borwsers = target for transpiling
+  ES6/2015 - ES 2023 are modern JS and well supported but dont know if all users are using modern browsers
+
+  future relases are ESNext need to go through 4 stages
+
+  */
